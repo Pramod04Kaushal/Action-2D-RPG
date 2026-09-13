13c43cee9a3e699c5482f62c1aebad86161479d0
Pramod Kaushal Fernando <pramodkaushalgd@gmail.com>
2026-09-13 11:39:30 +0530

    Enemy Animations

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Enemy_Movement : MonoBehaviour
 {
     public float speed;
-    private bool isChaseing;
+
+
+    private int facingDirection = -1;
+    private EnemyState enemyState, newState;
 
     private Rigidbody2D rb;
     private Transform player;
 
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        ChangeState(EnemyState.Idle);
+
     }
 
 
@@ -22,13 +30,25 @@ public class Enemy_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isChaseing)
+        if(enemyState == EnemyState.Chasing)
         {
+            if (player.position.x > transform.position.x && facingDirection == -1 || 
+                player.position.x < transform.position.x && facingDirection == 1)
+            {
+                Flip();
+            }
+
 
             Vector2 direction = (player.position - transform.position).normalized; // Player position - Enemy position
             rb.velocity = direction * speed; 
         }
 
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1; 
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -37,10 +57,11 @@ public class Enemy_Movement : MonoBehaviour
         {
             if (player == null)
             {
-                player = collision.transform;
+                player = collision.transform; 
             }
                
-            isChaseing = true;
+
+            ChangeState(EnemyState.Chasing);
         }
     }
 
@@ -48,6 +69,40 @@ public class Enemy_Movement : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        isChaseing = false;
+        if (collision.gameObject.tag == "Player")
+        {
+            rb.velocity = Vector2.zero;
+            ChangeState(EnemyState.Idle);
+        }
     }
+
+    void ChangeState(EnemyState newState)
+    {
+        // Exit current state
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isIdle", false);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isChasing", false);
+
+        // Enter new state
+        enemyState = newState;
+
+        //Update the new Animation
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isIdle", true);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isChasing", true);
+    }
+
+
+
 }
+
+public enum EnemyState
+{
+    Idle,
+    Chasing,
+
+}
+
+
