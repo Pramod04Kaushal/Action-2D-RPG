f875c545ee82cd5398100eafa67eedabe065e4fd
Pramod Kaushal Fernando <pramodkaushalgd@gmail.com>
2026-09-14 13:07:43 +0530

    Damaging Enemies

--- Assets/Scripts/Player_Combat.cs
+++ Assets/Scripts/Player_Combat.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player_Combat : MonoBehaviour
 {
+    public Transform attackPoint;
+    public float weaponRange = 1;
+    public LayerMask enemyLayer;
+    public int damage = 1;
+
     public Animator anim;
 
     public float cooldown = 0.1f;
@@ -21,16 +26,30 @@ public class Player_Combat : MonoBehaviour
         if (timer <= 0)
         {
             anim.SetBool("isAttacking", true);
+
             timer = cooldown;
         }
     }
 
+    public void DealDamage()
+    {
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, enemyLayer);
 
+        if (enemies.Length > 0)
+        {
+            enemies[0].GetComponent<Enemy_Health>().ChangeHealth(-damage);
+        }
+    }
 
     public void FinishAttack()
     {
         anim.SetBool("isAttacking", false);
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, weaponRange);
+    }
 
 }
