762d09cef2d67520b92db025f4e9bbd7791abf1b
Pramod Kaushal Fernando <pramodkaushalgd@gmail.com>
2026-09-21 13:08:24 +0530

    ExpSlider Update when press Return

---
+++ Assets/Scripts/Player Scripts/ExpManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ExpManager : MonoBehaviour
+{
+    public int level;
+    public int currentExp;
+    public int expToLevel = 10;
+    public float expGrowthMultiplier = 1.2f; //add 20% more EXP to level each time
+    public Slider expSlider;
+    public TMP_Text currentLevelText;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            GainExperience(2);
+        }
+    }
+
+    public void GainExperience(int amount)
+    {
+        currentExp += amount;
+
+        if (currentExp >= expToLevel)
+        {
+            LevelUp();
+        }
+
+        UpdateUI();
+    }
+
+    private void LevelUp()
+    {
+        level++;
+        currentExp -= expToLevel;
+        expToLevel = Mathf.RoundToInt(expToLevel * expGrowthMultiplier);
+    }
+
+    public void UpdateUI()
+    {
+        expSlider.maxValue = expToLevel;
+        expSlider.value = currentExp;
+        currentLevelText.text = "Level: " + level;
+    }
+}
